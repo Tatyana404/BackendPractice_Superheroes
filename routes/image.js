a4de8141769a584e61092c34c949df0eb230868d
Tatyana404 <kot.kate2020@gmail.com>
2021-04-12 02:21:53 +0300

    Refactor

--- routes/image.js
+++ routes/image.js
@@ -4,16 +4,16 @@ const multer = require('multer');
 const ImageController = require('../controller/image.controller');
 // const paginate = require('../middlewares/paginate.mw');
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, STATIC_PATH);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${Date.now()}.${file.originalname}`);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, '../public/images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.${file.originalname}`);
+  },
+});
 
-const upload = multer({ dest: path.resolve(__dirname, '../public/images') });
+const upload = multer({ storage });
 
 const imageRouter = Router();
 
