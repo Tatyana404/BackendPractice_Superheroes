969b6836da66366e46d8da91e9c997ca2688a564
Tatyana404 <kot.kate2020@gmail.com>
2021-04-12 02:44:43 +0300

    Add router

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/error.handlers.js');
 const app = express();
 
-app.use(express.json()); 
+app.use(express.static('public'));
+
+app.use(express.json());
 
 app.use('/api', router);
 app.use(errorHandler);
--- controller/image.controller.js
+++ controller/image.controller.js
@@ -1,9 +1,34 @@
 // const createError = require('http-errors');
-// const { Images } = require('../models');
+const { Images, Supergeroes } = require('../models/');
 
 module.exports.createImage = async (req, res, next) => {
   try {
-    res.send(req.file);
+    const {
+      file: { filename },
+      params: { id },
+      body,
+    } = req;
+
+    const [count, [updatedSupergero]] = await Supergeroes.update(
+      { imagePath: filename },
+      {
+        where: { id: id },
+        returning: true,
+      }
+    );
+
+    // await Superpowers.bulkCreate(
+    //   powerName.map(stringSuperpowers => ({
+    //     powerName: stringSuperpowers,
+    //     heroId: id,
+    //   })),
+    //   {
+    //     fields: ['powerName', 'heroId'],
+    //     returning: true,
+    //   }
+    // );
+
+    res.send(updatedSupergero);
   } catch (err) {
     next(err);
   }
