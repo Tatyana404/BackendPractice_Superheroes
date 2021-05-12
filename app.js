32046a834a6959c1643e7ef3f5bcb42c3b4c883a
Tatyana404 <kot.kate2020@gmail.com>
2021-05-12 13:17:32 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middlewares/error.handlers.js');
 const { STATIC_PATH } = require('./config/config');
 const app = express();
 
 app.use(express.static(STATIC_PATH));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
